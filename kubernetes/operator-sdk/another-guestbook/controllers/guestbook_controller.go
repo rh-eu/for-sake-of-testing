fd15deefda08bf0df6d0300579a009408281fe81
Rainer Helmes <rh@mifomm.eu>
2020-08-14 15:49:51 +0000

    testing operator sdk

---
+++ kubernetes/operator-sdk/another-guestbook/controllers/guestbook_controller.go
@@ -0,0 +1,99 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	webappv1 "github.com/rh/tutorial/Operator-SDK/another-guestbook/api/v1"
+)
+
+// GuestBookReconciler reconciles a GuestBook object
+type GuestBookReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=webapp.mifomm.eu,resources=guestbooks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=webapp.mifomm.eu,resources=guestbooks/status,verbs=get;update;patch
+
+// Reconcile defines ...
+func (r *GuestBookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("guestbook", req.NamespacedName)
+
+	log.Info("reconciling guestbook")
+
+	var book webappv1.GuestBook
+	if err := r.Get(ctx, req.NamespacedName, &book); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var redis webappv1.Redis
+	redisName := client.ObjectKey{Name: book.Spec.RedisName, Namespace: req.Namespace}
+	if err := r.Get(ctx, redisName, &redis); err != nil {
+		log.Error(err, "didn't get redis")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	log.Info("got redis", "redis", redis.Name)
+
+	deployment, err := r.desiredDeployment(book, redis)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	svc, err := r.desiredService(book)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	applyOpts := []client.PatchOption{client.ForceOwnership, client.FieldOwner("guestbook-controller")}
+
+	err = r.Patch(ctx, &deployment, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.Patch(ctx, &svc, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	book.Status.URL = urlForService(svc, book.Spec.Frontend.ServingPort)
+	log.Info("Status URL", "Status URL", book.Status.URL)
+
+	err = r.Status().Update(ctx, &book)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	log.Info("reconciled guestbook")
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager defines ...
+func (r *GuestBookReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&webappv1.GuestBook{}).
+		Complete(r)
+}
---
+++ kubernetes/operator-sdk/version-100/controllers/redis_controller.go
@@ -0,0 +1,111 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	webappv1 "github.com/rh/tutorial/Operator-SDK/version-100/api/v1"
+)
+
+// RedisReconciler reconciles a Redis object
+type RedisReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=webapp.mifomm.eu,resources=redis,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=webapp.mifomm.eu,resources=redis/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+
+// Reconcile defines ...
+func (r *RedisReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("redis", req.NamespacedName)
+
+	log.Info("reconciling redis")
+
+	var redis webappv1.Redis
+	if err := r.Get(ctx, req.NamespacedName, &redis); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	leaderDepl, err := r.leaderDeployment(redis)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	leaderSvc, err := r.desiredService(redis, "leader")
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	followerDepl, err := r.followerDeployment(redis)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	followerSvc, err := r.desiredService(redis, "follower")
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	applyOpts := []client.PatchOption{client.ForceOwnership, client.FieldOwner("redis-controller")}
+
+	err = r.Patch(ctx, &leaderDepl, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	err = r.Patch(ctx, &leaderSvc, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	err = r.Patch(ctx, &followerDepl, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	err = r.Patch(ctx, &followerSvc, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	redis.Status.LeaderService = leaderSvc.Name
+	redis.Status.FollowerService = followerSvc.Name
+
+	if err := r.Status().Update(ctx, &redis); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	log.Info("reconciled redis")
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager defines ...
+func (r *RedisReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&webappv1.Redis{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
